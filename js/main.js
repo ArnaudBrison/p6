97666482f58e03dfb2b70dde9de94496852cdf70
ArnaudBrison <arnaud.brison@orange.fr>
2018-05-07 16:08:32 +0200

    Revoir la fonction de check de joueur au placement du joueur 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //initialisation du plateau
 plateau_fonction.init_plateau();
+//placement des rochers
+plateau_fonction.send_rocher_to_placement();
 //initialisation des armes
 var baton = new armes.init("baton", 10);
 var fleau = new armes.init("fleau", 40);
@@ -9,11 +11,11 @@ var sabre_laser = new armes.init("sabre_laser", 70);
 //envoie des armes dans le tableau
 armes.tab_armes.push(fleau, masse, hache, sabre_laser, baton);
 // mise en place des arme
-plateau_fonction.placement_arme();
+plateau_fonction.send_arme_to_placement();
 //initialisation des joueurs
 var joueur1 = new joueur.init("joueur1");
 var joueur2 = new joueur.init("joueur2");
 //mise en place du joueur 1
-plateau_fonction.placement_joueur1();
+plateau_fonction.send_joueur1_to_placement();
 //mise en place du joueur 2
 plateau_fonction.placement_joueur2();
--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,4 @@
 var ligne_max = 9;
-var case_par_ligne_max = 10;
-var case_totale = ligne_max * case_par_ligne_max;
+var colonne_max = 10;
+var case_totale = ligne_max * colonne_max;
+var max_rocher = 15;

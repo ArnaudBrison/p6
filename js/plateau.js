6965c5836651bab6651474e57e5763cd34dd369f
ArnaudBrison <arnaud.brison@orange.fr>
2018-05-17 15:00:23 +0200

    petite modif graphique

--- js/plateau.js
+++ js/plateau.js
@@ -78,11 +78,12 @@ var plateau_fonction = {
 
   init_info_joueur : function(joueur){
     $('#tab_' + joueur.nom).empty();
-    $('<p/>').append('nom: ' + joueur.nom).appendTo('#tab_' + joueur.nom);
+    $('<p/>').append(joueur.nom.toUpperCase()).appendTo('#tab_' + joueur.nom);
     $('<p/>').addClass('case ' + joueur.nom +'_tab').appendTo('#tab_' + joueur.nom);
-    $('<p/>').append('sante: ' + joueur.sante).appendTo('#tab_' + joueur.nom);
-    $('<p/>').append('arme: ' + joueur.arme).appendTo('#tab_' + joueur.nom);
-    $('<p/>').append('degats: ' + joueur.degats).appendTo('#tab_' + joueur.nom);
+    $('<p/>').append('Sante: ' + joueur.sante).appendTo('#tab_' + joueur.nom);
+    $('<p/>').append('Arme: ' + joueur.arme).appendTo('#tab_' + joueur.nom);
+    $('<p/>').addClass('case ' + joueur.arme).appendTo('#tab_' + joueur.nom);
+    $('<p/>').append('Degats: ' + joueur.degats).appendTo('#tab_' + joueur.nom);
     var div_bouton = $('<div/>').addClass('div_bouton').appendTo('#tab_' + joueur.nom);
     var bouton_attq = $('<a/>').addClass('waves-effect waves-light btn-small attq_def_btn  red').append('Attaquer').appendTo(div_bouton).hide();
     $('<i/>').addClass('material-icons right').append('restaurant').appendTo(bouton_attq);
